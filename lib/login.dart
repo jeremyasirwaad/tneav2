6122995685b994f48b548629f1a5e0fcaab46788
Jeremy <asirwaadj@gmail.com>
2022-04-27 13:21:06 +0530

    added login with auth

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+import 'dart:html';
+import './datamodel/userdata.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import './dashboard.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:http/http.dart' as http;
 
 class loginpage extends StatefulWidget {
   @override
@@ -9,6 +13,41 @@ class loginpage extends StatefulWidget {
 }
 
 class _loginpageState extends State<loginpage> {
+  final _formKey = GlobalKey<FormState>();
+  bool hidepass = true;
+  String? email;
+  String? password;
+  var errorsg = "";
+  late Userdata user;
+  Future save() async {
+    var res = await http.post(Uri.parse("http://localhost:5000/login"),
+        headers: <String, String>{
+          'Context-Type': 'application/json;charSet=UTF-8'
+        },
+        body: <String, dynamic>{
+          "email": email,
+          "password": password
+        });
+
+    var data = jsonDecode(res.body);
+    if (!data['status']) {
+      setState(() {
+        errorsg = data['message'];
+      });
+    }
+    if (data['status']) {
+      //  user = Userdata.fromJson(data);
+      print(data);
+      Navigator.pushReplacement(
+          context,
+          PageTransition(
+              type: PageTransitionType.scale,
+              alignment: Alignment.bottomCenter,
+              child: dashboard()));
+    }
+    // print(res.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -103,37 +142,82 @@ class _loginpageState extends State<loginpage> {
                           //   decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey.shade200)))),
                           // )
 
-                          Container(
-                            child: Column(
-                              children: [
-                                Container(
-                                  decoration: BoxDecoration(
-                                      border: Border(
-                                          bottom: BorderSide(
-                                              color: Colors.grey.shade100))),
-                                  child: Padding(
+                          Form(
+                            key: _formKey,
+                            child: Container(
+                              child: Column(
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                        border: Border(
+                                            bottom: BorderSide(
+                                                color: Colors.grey.shade100))),
+                                    child: Padding(
+                                      padding: EdgeInsets.only(
+                                          top: 8,
+                                          left: 20,
+                                          right: 20,
+                                          bottom: 8),
+                                      child: TextFormField(
+                                          controller: TextEditingController(
+                                              text: email),
+                                          onChanged: (value) {
+                                            email = value;
+                                          },
+                                          validator: (String? value) {
+                                            if (value != null &&
+                                                value.isEmpty) {
+                                              return "Enter Email";
+                                            } else if (RegExp(
+                                                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                                .hasMatch(value as String)) {
+                                              return null;
+                                            } else {
+                                              return "Enter Valid Email";
+                                            }
+                                          },
+                                          decoration: InputDecoration(
+                                              border: InputBorder.none,
+                                              hintText: "Email",
+                                              hintStyle: TextStyle(
+                                                  color:
+                                                      Colors.grey.shade500))),
+                                    ),
+                                  ),
+                                  Padding(
                                     padding: EdgeInsets.only(
                                         top: 8, left: 20, right: 20, bottom: 8),
-                                    child: TextField(
+                                    child: TextFormField(
+                                        controller: TextEditingController(
+                                            text: password),
+                                        onChanged: (value) {
+                                          password = value;
+                                        },
+                                        validator: (String? value) {
+                                          if (value != null && value.isEmpty) {
+                                            return "Enter Password";
+                                          } else {
+                                            return null;
+                                          }
+                                        },
+                                        obscureText: hidepass,
                                         decoration: InputDecoration(
+                                            suffixIcon: IconButton(
+                                                onPressed: () {
+                                                  setState(() {
+                                                    hidepass = !hidepass;
+                                                  });
+                                                },
+                                                icon: Icon(hidepass
+                                                    ? Icons.visibility_off
+                                                    : Icons.visibility)),
                                             border: InputBorder.none,
-                                            hintText: "Email",
+                                            hintText: "Password",
                                             hintStyle: TextStyle(
                                                 color: Colors.grey.shade500))),
                                   ),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(
-                                      top: 8, left: 20, right: 20, bottom: 8),
-                                  child: TextField(
-                                      obscureText: true,
-                                      decoration: InputDecoration(
-                                          border: InputBorder.none,
-                                          hintText: "Password",
-                                          hintStyle: TextStyle(
-                                              color: Colors.grey.shade500))),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
                           ),
                         ]),
@@ -150,12 +234,12 @@ class _loginpageState extends State<loginpage> {
                       ),
                       GestureDetector(
                         onTap: () => {
-                          Navigator.pushReplacement(
-                              context,
-                              PageTransition(
-                                  type: PageTransitionType.scale,
-                                  alignment: Alignment.bottomCenter,
-                                  child: dashboard()))
+                          if (_formKey.currentState!.validate())
+                            {
+                              save(),
+                            }
+                          else
+                            {print("not ok")}
                         },
                         child: Container(
                             height: 50,
